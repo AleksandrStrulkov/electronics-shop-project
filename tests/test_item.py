8a36dd85e5a3c96b6eabab1f41f3304e05c01997
AleksandrStrulkov <astrulkov@yandex.ru>
2023-09-09 09:17:26 +0300

    Update item.py and test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,12 +5,12 @@ from src.item import Item
 
 @pytest.fixture
 def test_item():
-	return Item(['смартфон', 20000, 10])
+	return Item('смартфон', 20000, 10)
 
 
 def test_calculate_total_price(test_item):
-	assert test_item.calculate_total_price() == 12500*10
+	assert test_item.calculate_total_price() == 200000
 
 
-def test_apply_discount(test_item):
-	assert test_item.apply_discout() == 8000
+
+
