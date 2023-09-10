c432077606950e4f5552018095e3b734142f9274
AleksandrStrulkov <astrulkov@yandex.ru>
2023-09-10 20:33:25 +0300

    Add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -4,13 +4,33 @@ from src.item import Item
 
 
 @pytest.fixture
-def test_item():
+def item():
 	return Item('смартфон', 20000, 10)
 
 
-def test_calculate_total_price(test_item):
-	assert test_item.calculate_total_price() == 200000
+def test_item_initialized(item):
+	assert item.name == "смартфон"
+	assert item.price == 20000
+	assert item.quantity == 10
 
 
+def test_calculate_total_price(item):
+	assert item.calculate_total_price() == 200000
+
+
+def test_apply_discount(item) -> None:
+	Item.pay_rate = 0.8
+	item.apply_discount()
+	assert item.price == 16000
+
+
+def test_name(item):
+	item.name = 'Суперсмартфон'
+	assert item.name == 'Суперсмарт'
+
+
+def test_string_to_number(item):
+	assert Item.string_to_number('7') == 7
+	assert Item.string_to_number('7.5') == 7
 
 
