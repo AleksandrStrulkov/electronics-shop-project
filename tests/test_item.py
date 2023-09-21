322c5e5b7583ebbbf107a9b4f4e330fb8b660164
AleksandrStrulkov <astrulkov@yandex.ru>
2023-09-21 20:39:47 +0300

    Add test class Phone

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,44 +1,34 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
-
+from src.phone import Phone
 
 @pytest.fixture
 def item():
 	return Item('смартфон', 20000, 10)
 
 
-def test_repr(item):
+def test_item(item):
 	assert repr(item) == "Item('смартфон', 20000, 10)"
-
-
-def test_str(item):
 	assert str(item) == 'смартфон'
-
-
-def test_item_initialized(item):
 	assert item.name == "смартфон"
 	assert item.price == 20000
 	assert item.quantity == 10
-
-
-def test_calculate_total_price(item):
 	assert item.calculate_total_price() == 200000
-
-
-def test_apply_discount(item) -> None:
 	Item.pay_rate = 0.8
 	item.apply_discount()
 	assert item.price == 16000
-
-
-def test_name(item):
 	item.name = 'Суперсмартфон'
 	assert item.name == 'Суперсмарт'
-
-
-def test_string_to_number(item):
 	assert Item.string_to_number('7') == 7
 	assert Item.string_to_number('7.5') == 7
 
 
+@pytest.fixture
+def phone():
+	return Phone("iPhone 14", 120_000, 5, 2)
+
+
+def test_phone(phone):
+	assert repr(phone) == "Phone:(iPhone 14, 120000, 5, 2)"
+	assert str(phone) == 'iPhone 14'
