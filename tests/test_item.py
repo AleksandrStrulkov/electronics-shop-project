4fb670651d7881e36d7e949744242f08ef5fed84
AleksandrStrulkov <astrulkov@yandex.ru>
2023-10-02 21:15:17 +0300

    Add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
-from src.item import Item
+from src.item import Item, InstantiateCSVError
 from src.phone import Phone
 
 
@@ -25,4 +25,14 @@ def test_item(item):
 	assert Item.string_to_number('7.5') == 7
 
 
+def test_empty_csv():
+	"""Тестирование пустого файла"""
+	with pytest.raises(FileNotFoundError):
+		Item.instantiate_from_csv("item.csv")
+
+
+def test_broken_csv():
+	"""Тестирование поврежденного файла"""
+	with pytest.raises(InstantiateCSVError):
+		Item.instantiate_from_csv("../src/item.csv")
 
